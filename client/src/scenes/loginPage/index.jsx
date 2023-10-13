4e4062171f3a0c36541ad9276edb0e87a852c684
Mitchell Hay <haym23@yahoo.com>
2023-10-12 22:17:58 -0400

    Add welcome message

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -4,8 +4,12 @@ import Form from './Form';
 const LoginPage = () => {
 
   return (
-    <div className="flex justify-center items-center h-screen bg-background-light">
-      <div className="bg-background-dark shadow-md rounded px-8 py-6">
+    <div className="flex flex-col items-center h-screen bg-background-light">
+      <div className="flex w-full max-w-xs flex-col text-center pt-2 pb-20">
+        <div className="text-lg font-bold text-light-text">Welcome to ResumeTitan!</div>
+        <div className="text-light-text">Using AI to help build your resume</div>
+      </div>
+      <div className="justify-center bg-background-dark shadow-md rounded px-8 py-6">
         <Form />
       </div>
     </div>
